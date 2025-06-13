2ed655302827baf774420a273810d31cf8d49952
dunewalkin <sergei.gunchenko@gmail.com>
2025-06-13 22:49:12 +0300

    update to new ui

--- source/js/main/map.js
+++ source/js/main/map.js
@@ -1,21 +1,58 @@
+// "use strict";
+// (function () {
+//    const paths = document.querySelectorAll("#locations path[data-index]");
+
+//    paths.forEach((path) => {
+//      const index = path.getAttribute("data-index");
+ 
+//      path.addEventListener("mouseenter", () => {
+//        const info = document.querySelector(`.locations-info[data-index="${index}"]`);
+//        path.classList.add("hovered");
+//        info?.classList.add("hovered");
+//      });
+ 
+//      path.addEventListener("mouseleave", () => {
+//        const info = document.querySelector(`.locations-info[data-index="${index}"]`);
+//        path.classList.remove("hovered");
+//        info?.classList.remove("hovered");
+//      });
+//    });
+
+// })();
+
+
+
 "use strict";
 (function () {
    const paths = document.querySelectorAll("#locations path[data-index]");
 
    paths.forEach((path) => {
      const index = path.getAttribute("data-index");
- 
+
      path.addEventListener("mouseenter", () => {
        const info = document.querySelector(`.locations-info[data-index="${index}"]`);
+       const locItem = document.querySelector(`.loc-item[data-index="${index}"]`);
+       const locNumber = locItem?.querySelector(".loc-number");
+       const locDesc = locItem?.querySelector(".loc-desc");
+
        path.classList.add("hovered");
        info?.classList.add("hovered");
+       locNumber?.classList.add("hovered");
+       locDesc?.classList.add("show");
      });
- 
+
      path.addEventListener("mouseleave", () => {
        const info = document.querySelector(`.locations-info[data-index="${index}"]`);
+       const locItem = document.querySelector(`.loc-item[data-index="${index}"]`);
+       const locNumber = locItem?.querySelector(".loc-number");
+       const locDesc = locItem?.querySelector(".loc-desc");
+
        path.classList.remove("hovered");
        info?.classList.remove("hovered");
+       locNumber?.classList.remove("hovered");
+       locDesc?.classList.remove("show");
      });
    });
-
 })();
+
+
