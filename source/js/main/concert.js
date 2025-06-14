2bb9d4d82069681e24ff56dd2436fdb5aa0798e0
Samsonov Egeny <evgeniy.samsonov@sbermegamarket.ru>
2025-06-14 17:53:45 +0300

    content fixes

--- source/js/main/concert.js
+++ source/js/main/concert.js
@@ -1,26 +1,29 @@
 "use strict";
 (function () {
-   const concertSwiper = new Swiper(".js-concert-swiper-container", {
-      slidesPerView: 4,
-      spaceBetween: 40,
-      navigation: {
-        nextEl: ".concert-button-next",
-        prevEl: ".concert-button-prev",
+  const vw = window.innerWidth;
+  if (vw >= 768) {
+    return;
+  }
+  const concertSwiper = new Swiper(".js-concert-swiper-container", {
+    slidesPerView: 4,
+    spaceBetween: 40,
+    navigation: {
+      nextEl: ".concert-button-next",
+      prevEl: ".concert-button-prev",
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 40,
       },
-      breakpoints: {
-         1024: {
-            slidesPerView: 4,
-            spaceBetween: 40,
-         },
-        768: {
-         slidesPerView: 2,
-         spaceBetween: 40,
-        },
-        0: {
-          slidesPerView: 1,
-          spaceBetween: 20,
-        },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
       },
-    });
- 
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+    },
+  });
 })();
