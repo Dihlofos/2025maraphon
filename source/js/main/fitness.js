8324e51306a046094c2911545942d4031f5b6940
Samsonov Egeny <evgeniy.samsonov@sbermegamarket.ru>
2025-06-14 08:05:52 +0300

    content and maps fixes

--- source/js/main/fitness.js
+++ source/js/main/fitness.js
@@ -1,96 +1,94 @@
 "use strict";
 (function () {
-   const fitnessSwiper = new Swiper(".js-fitness-swiper-container", {
-      slidesPerView: 3,
-      spaceBetween: 20,
-      allowTouchMove: false, 
-      centeredSlides: false,
-      navigation: {
-        nextEl: ".fitness-button-next",
-        prevEl: ".fitness-button-prev",
+  const fitnessSwiper = new Swiper(".js-fitness-swiper-container", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    allowTouchMove: false,
+    centeredSlides: false,
+    navigation: {
+      nextEl: ".fitness-button-next",
+      prevEl: ".fitness-button-prev",
+    },
+    breakpoints: {
+      1280: {
+        slidesPerView: 3,
+        spaceBetween: 20,
       },
-      breakpoints: {
-         
-         
-         1280: {
-            slidesPerView: 3,
-            spaceBetween: 20,
-          },
-          900: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          0: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-            centeredSlides: true,
-          },
-        
-        
+      900: {
+        slidesPerView: 2,
+        spaceBetween: 20,
       },
-    });
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        centeredSlides: true,
+      },
+    },
+  });
 
-   // const fitnessSwiper = new Swiper(".js-fitness-swiper-container", {
-   //    // Базово — 1 слайд
-   //    slidesPerView: 1,
-   //    spaceBetween: 20,          // отступ между слайдами и от кнопок ≤ 20px
-   //    allowTouchMove: false,     // свайп отключён
-   //    centeredSlides: true,     // без центрирования
-   //    navigation: {
-   //      nextEl: ".fitness-button-next",
-   //      prevEl: ".fitness-button-prev",
-   //    },
-   //    breakpoints: {
-   //      // от 1024 до 1279px — 2 слайда
-   //      1024: {
-   //        slidesPerView: 2,
-   //        spaceBetween: 20
-   //      },
-   //      // от 1280px и выше — 3 слайда
-   //      1280: {
-   //        slidesPerView: 3,
-   //        spaceBetween: 20
-   //      }
-   //    }
-   //  });
-    
+  // const fitnessSwiper = new Swiper(".js-fitness-swiper-container", {
+  //    // Базово — 1 слайд
+  //    slidesPerView: 1,
+  //    spaceBetween: 20,          // отступ между слайдами и от кнопок ≤ 20px
+  //    allowTouchMove: false,     // свайп отключён
+  //    centeredSlides: true,     // без центрирования
+  //    navigation: {
+  //      nextEl: ".fitness-button-next",
+  //      prevEl: ".fitness-button-prev",
+  //    },
+  //    breakpoints: {
+  //      // от 1024 до 1279px — 2 слайда
+  //      1024: {
+  //        slidesPerView: 2,
+  //        spaceBetween: 20
+  //      },
+  //      // от 1280px и выше — 3 слайда
+  //      1280: {
+  //        slidesPerView: 3,
+  //        spaceBetween: 20
+  //      }
+  //    }
+  //  });
 
-   const sliderItems = document.querySelectorAll(".fitness__slider-item-image-wrapper");
-   const scheduleWrappers = document.querySelectorAll(".schedule-fitness-wrapper");
-   
-   sliderItems.forEach((item) => {
-      item.addEventListener("click", () => {
+  const sliderItems = document.querySelectorAll(
+    ".fitness__slider-item-image-wrapper"
+  );
+  if (!sliderItems.length) return;
+  const scheduleWrappers = document.querySelectorAll(
+    ".schedule-fitness-wrapper"
+  );
+
+  sliderItems.forEach((item) => {
+    item.addEventListener("click", () => {
       const id = item.getAttribute("data-id");
-   
+
       scheduleWrappers.forEach((el) => {
-         if (el.getAttribute("data-id") === id) {
-            el.style.display = "flex";
-         } else {
-            el.style.display = "none";
-         }
+        if (el.getAttribute("data-id") === id) {
+          el.style.display = "flex";
+        } else {
+          el.style.display = "none";
+        }
       });
-   
-      sliderItems.forEach(i => i.classList.remove("active"));
+
+      sliderItems.forEach((i) => i.classList.remove("active"));
       item.classList.add("active");
-      });
-   });
-   
-   if (sliderItems[0]) {
-      sliderItems[0].click();
-   }
-
-   document.addEventListener("DOMContentLoaded", () => {
-      const slides = document.querySelectorAll(".fitness__slider-item");
-      const prevButton = document.querySelector(".fitness-button-prev");
-      const nextButton = document.querySelector(".fitness-button-next");
-    
-      const isDesktop = window.innerWidth >= 1024;
-    
-      if (isDesktop && slides.length < 4) {
-         prevButton.style.display = "none";
-         nextButton.style.display = "none";
-      } 
     });
-    
+  });
 
+  if (sliderItems[0]) {
+    sliderItems[0].click();
+  }
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const slides = document.querySelectorAll(".fitness__slider-item");
+    const prevButton = document.querySelector(".fitness-button-prev");
+    const nextButton = document.querySelector(".fitness-button-next");
+
+    const isDesktop = window.innerWidth >= 1024;
+
+    if (isDesktop && slides.length < 4) {
+      prevButton.style.display = "none";
+      nextButton.style.display = "none";
+    }
+  });
 })();
--- source/js/main/stages.js
+++ source/js/main/stages.js
@@ -1,23 +1,22 @@
 "use strict";
 (function () {
-  const stagebuttons = document.querySelectorAll('.stage-btn');
-  const stageContents = document.querySelectorAll('.stage-content');
+  const stagebuttons = document.querySelectorAll(".stage-btn");
+  if (!stagebuttons.length) return;
+  const stageContents = document.querySelectorAll(".stage-content");
 
   if (!stagebuttons.length || !stageContents.length) return;
 
-  stagebuttons.forEach(btn => {
-    btn.addEventListener('click', () => {
-      const target = btn.getAttribute('data-stage');
+  stagebuttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const target = btn.getAttribute("data-stage");
 
-      stagebuttons.forEach(b => b.classList.remove('active'));
-      btn.classList.add('active');
+      stagebuttons.forEach((b) => b.classList.remove("active"));
+      btn.classList.add("active");
 
-      stageContents.forEach(content => content.classList.remove('active'));
+      stageContents.forEach((content) => content.classList.remove("active"));
 
       const targetContent = document.querySelector(`.stage-content--${target}`);
-      if (targetContent) targetContent.classList.add('active');
+      if (targetContent) targetContent.classList.add("active");
     });
-
   });
 })();
-
