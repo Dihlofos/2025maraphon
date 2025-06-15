2395313a584ba5ab2122809cc2cb34b7291ccf19
Samsonov Egeny <evgeniy.samsonov@sbermegamarket.ru>
2025-06-15 23:24:53 +0300

    add metrics

--- build/js/main.js
+++ build/js/main.js
@@ -43,6 +43,26 @@
   });
 })();
 
+"use strict";
+(function () {
+  const key = "maraphon-cookie-modal-shown";
+  let modal = document.querySelector(".js-cookie");
+  if (!modal) {
+    return;
+  }
+
+  let closeButton = modal.querySelector(".js-cookie-close");
+
+  if (!window.localStorage.getItem(key)) {
+    modal.classList.remove("hidden");
+  }
+
+  closeButton.addEventListener("click", () => {
+    modal.classList.add("hidden");
+    window.localStorage.setItem(key, true);
+  });
+})();
+
 // "use strict";
 // (function () {
 //   const dropdowns = document.querySelectorAll(".js-dropdown");
